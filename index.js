1f40d82021c4a003a738687b8bf20754624cf130
STNevios <jealous91@outlook.com>
2016-12-07 15:34:20 +0100

    Menu instruments avancé

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ function nameInstrument(name) {
 	audio.play();
 	var div = document.getElementById('nameInstrument');
 	if (name == 1)
-		div.innerHTML = 'Baguette';
+		div.innerHTML = 'Baton';
 	else if (name == 2) {
 		div.innerHTML = 'Ocarina';
 	}
 	else if (name == 3) {
-		div.innerHTML = 'Harpe';
+		div.innerHTML = 'Harp';
 	}
 }
 
@@ -39,54 +39,68 @@ function choose_ocarina() {
 	var audio = new Audio('ressources/TP_SaveMenu_SlotSelect.wav');
 	audio.play();
 	var body = document.querySelector('body');
-	body.innerHTML = '<div class="you_choose">You have chosen the Ocarina<div id="navi" onclick="ocarina()"><img id="navi_img" src="ressources/navi.png"><span>Continue<span></div></div>';
+	body.innerHTML = '<div class="you_choose"><span id="you_have_chosen">You have chosen the Ocarina</span><div id="navi" onclick="ocarina()"><img id="navi_img" src="ressources/navi.png"><span>Continue</span></div></div>';
 }
 
 function choose_harp() {
 	var audio = new Audio('ressources/TP_SaveMenu_SlotSelect.wav');
 	audio.play();
 	var body = document.querySelector('body');
-	body.innerHTML = '<div class="you_choose">You have chosen the Harp<div id="navi" onclick="harp()"><img id="navi_img" src="ressources/navi.png"><span>Continue<span></div></div>';
+	body.innerHTML = '<div class="you_choose"><span id="you_have_chosen">You have chosen the Harp</span><div id="navi" onclick="harp()"><img id="navi_img" src="ressources/navi.png"><span>Continue</span></div></div>';
 }
 
 function choose_wand() {
 	var audio = new Audio('ressources/TP_SaveMenu_SlotSelect.wav');
 	audio.play();
 	var body = document.querySelector('body');
-	body.innerHTML = '<div class="you_choose">You have chosen the Wand<div id="navi" onclick="wand()"><img id="navi_img" src="ressources/navi.png"><span>Continue<span></div></div>';
+	body.innerHTML = '<div class="you_choose"><span id="you_have_chosen">You have chosen the Baton</span><div id="navi" onclick="wand()"><img id="navi_img" src="ressources/navi.png"><span>Continue</span></div></div>';
 }
 
 function ocarina() {
 	var audio = new Audio('ressources/TP_SaveMenu_SlotSelect.wav');
 	audio.play();
-	console.log('Ocarina');
 	var body = document.querySelector('body');
-	body.innerHTML = '<div class="you_choose">' +
-	'<span onclick="quit()">Revenir au menu de départ</span>' + 
+	body.innerHTML = '<div class="you_choose">' + 
+	'<div class="gamescreen" id="gamescreen">' + 
+	'</div>' + 
+	'<div class="navbar">' + 
+	'<div id="navbar_sheets"><span onclick="display_sheets_ocarina()">Sheets</span></div>' + 
+	'<div id="quit"><span onclick="quit()">Return to title screen</span></div>' + 
+	'</div>' + 
 	'</div>';
 }
 
 function wand() {
 	var audio = new Audio('ressources/TP_SaveMenu_SlotSelect.wav');
 	audio.play();
-	console.log('Wand');
 	var body = document.querySelector('body');
-	body.innerHTML = '<div class="you_choose">' +
-	'<span onclick="quit()">Revenir au menu de départ</span>' + 
+	body.innerHTML = '<div class="you_choose">' + 
+	'<div class="gamescreen" id="gamescreen">' + 
+	'</div>' + 
+	'<div class="navbar">' + 
+	'<div id="navbar_sheets"><span onclick="display_sheets_wand()">Sheets</span></div>' + 
+	'<div id="quit"><span onclick="quit()">Return to title screen</span></div>' + 
+	'</div>' + 
 	'</div>';
 }
 
 function harp() {
 	var audio = new Audio('ressources/TP_SaveMenu_SlotSelect.wav');
 	audio.play();
-	console.log('Harp');
 	var body = document.querySelector('body');
-	body.innerHTML = '<div class="you_choose">' +
-	'<span onclick="quit()">Revenir au menu de départ</span>' + 
+	body.innerHTML = '<div class="you_choose">' + 
+	'<div class="gamescreen" id="gamescreen">' + 
+	'</div>' + 
+	'<div class="navbar">' + 
+	'<div id="navbar_sheets"><span onclick="display_sheets_harp()">Sheets</span></div>' + 
+	'<div id="quit"><span onclick="quit()">Return to title screen</span></div>' + 
+	'</div>' + 
 	'</div>';
 }
 
 function quit() {
+	var span = document.getElementById('quit');
+	span.outerHTML = '<div id="quit"><span>Return to title screen</span></div>';
 	var audio = new Audio('ressources/TP_SaveMenu_Continue_No.wav');
 	audio.play();
 	setTimeout(function() { restore(); }, 1000);
@@ -95,4 +109,21 @@ function quit() {
 function restore() {
 	var body = document.querySelector('body');
 	body.innerHTML = first_body;
+}
+
+function display_sheets_ocarina() {
+	var audio = new Audio('ressources/TP_SaveMenu_SlotSelect.wav');
+	audio.play();
+	var screen = document.getElementById('gamescreen');
+	screen.innerHTML = '<img id="sheets" src="ressources/sheets.jpg">';
+	var navbar = document.getElementById('navbar_sheets');
+	navbar.innerHTML = '<div id="navbar_sheets"><span onclick="ocarina()">Return</span></div>';
+}
+
+function display_sheets_wand() {
+
+}
+
+function display_sheets_harp() {
+
 }
