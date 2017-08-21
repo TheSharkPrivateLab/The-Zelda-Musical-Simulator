b8f279f218d30c83dd3c10b70fd4bf9b5b7ebd36
ALMERYS\ccameron <cameron.carra@almerys.com>
2017-08-21 17:41:07 +0200

    mise à niveau jquery début

--- ressources/js/index.js
+++ ressources/js/index.js
@@ -1,67 +1,67 @@
 //Menus
 
 function Menu() {
-    first_body = document.querySelector('body').innerHTML;
-    $("#triforce-left").animate({left: '-=10%'});
-    $("#triforce-right").animate({left: '+=10%'});
-    var audio = new Audio('ressources/audio/secret.wav');
+    var first_body = $("body").html();
+    $("#triforce-left").animate({left: "-=10%"});
+    $("#triforce-right").animate({left: "+=10%"});
+    var audio = new Audio("ressources/audio/secret.wav");
     audio.play();
     setTimeout(displayMenu, 500);
 }
 
 function displayMenu() {
     var Menu = $("#contain_menu");
-    menu = document.createElement('div');
-    menu.id = 'menu';
+    menu = document.createElement("div");
+    menu.id = "menu";
     Menu.append(menu);
     var Ocarina = "Ocarina";
     var Harpe = "Harpe";
     var Baguette = "Baguette";
-    menu.innerHTML = '<div class="instrument_div" id="instrument_div_left"><img id="wand" src="ressources/img/wand.png" onmouseover="nameInstrument(1)" onclick="choose_wand()"></div>' +
-        '<div class="instrument_div" id="instrument_div_middle"><h3 id="nameInstrument"></h3><img id="ocarina" src="ressources/img/ocarina.png" onmouseover="nameInstrument(2)" onclick="choose_ocarina()"></div>' +
-        '<div class="instrument_div" id="instrument_div_right"><img id="harp" src="ressources/img/harp.png" onmouseover="nameInstrument(3)" onclick="choose_harp()"></div>';
+    menu.innerHTML = "<div class=\"instrument_div\" id=\"instrument_div_left\"><img id=\"wand\" src=\"ressources/img/wand.png\" onmouseover=\"nameInstrument(1)\" onclick=\"choose_wand()\"></div>" +
+        "<div class=\"instrument_div\" id=\"instrument_div_middle\"><h3 id=\"nameInstrument\"></h3><img id=\"ocarina\" src=\"ressources/img/ocarina.png\" onmouseover=\"nameInstrument(2)\" onclick=\"choose_ocarina()\"></div>" +
+        "<div class=\"instrument_div\" id=\"instrument_div_right\"><img id=\"harp\" src=\"ressources/img/harp.png\" onmouseover=\"nameInstrument(3)\" onclick=\"choose_harp()\"></div>";
 }
 
 function nameInstrument(name) {
     var audio = new Audio('ressources/audio/TP_PauseMenu_Cursor1.wav');
     audio.play();
-    var div = document.getElementById('nameInstrument');
-    if (name == 1)
-        div.innerHTML = 'Baton';
-    else if (name == 2) {
-        div.innerHTML = 'Ocarina';
-    }
-    else if (name == 3) {
-        div.innerHTML = 'Harp';
+    var div = $("#nameInstrument");
+    switch (name) {
+        case 1:
+            div.html("Baton");
+            break;
+        case 2:
+            div.html("Ocarina");
+            break;
+        case 3:
+            div.html("Harp");
+            break;
     }
 }
 
 function choose_ocarina() {
-    var audio = new Audio('ressources/audio/TP_SaveMenu_SlotSelect.wav');
+    var audio = new Audio("ressources/audio/TP_SaveMenu_SlotSelect.wav");
     audio.play();
-    var body = document.querySelector('body');
-    body.innerHTML = '<div class="you_choose"><span id="you_have_chosen">You have chosen the Ocarina</span><div id="navi" onclick="ocarina()"><img id="navi_img" src="ressources/navi.png"><span>Continue</span></div></div>';
+    $("body").html("<div class=\"you_choose\"><span id=\"you_have_chosen\">You have chosen the Ocarina</span><div id=\"navi\" onclick=\"ocarina()\"><img id=\"navi_img\" src=\"ressources/navi.png\"><span>Continue</span></div></div>");
 }
 
 function choose_harp() {
-    var audio = new Audio('ressources/audio/TP_SaveMenu_SlotSelect.wav');
+    var audio = new Audio("ressources/audio/TP_SaveMenu_SlotSelect.wav");
     audio.play();
-    var body = document.querySelector('body');
-    body.innerHTML = '<div class="you_choose"><span id="you_have_chosen">You have chosen the Harp</span><div id="navi" onclick="harp()"><img id="navi_img" src="ressources/navi.png"><span>Continue</span></div></div>';
+    $("body").html("<div class=\"you_choose\"><span id=\"you_have_chosen\">You have chosen the Harp</span><div id=\"navi\" onclick=\"harp()\"><img id=\"navi_img\" src=\"ressources/navi.png\"><span>Continue</span></div></div>");
 }
 
 function choose_wand() {
-    var audio = new Audio('ressources/audio/TP_SaveMenu_SlotSelect.wav');
+    var audio = new Audio("ressources/audio/TP_SaveMenu_SlotSelect.wav");
     audio.play();
-    var body = document.querySelector('body');
-    body.innerHTML = '<div class="you_choose"><span id="you_have_chosen">You have chosen the Baton</span><div id="navi" onclick="wand()"><img id="navi_img" src="ressources/navi.png"><span>Continue</span></div></div>';
+    $("body").html("<div class=\"you_choose\"><span id=\"you_have_chosen\">You have chosen the Wand</span><div id=\"navi\" onclick=\"wand()\"><img id=\"navi_img\" src=\"ressources/navi.png\"><span>Continue</span></div></div>");
 }
 
 function quit() {
     window.removeEventListener("keydown", play_ocarina);
     var span = document.getElementById('quit');
-    span.outerHTML = '<div id="quit"><span>Return to title screen</span></div>';
-    var audio = new Audio('ressources/audio/TP_SaveMenu_Continue_No.wav');
+    $("#quit").html("<span>Return to title screen</span>");
+    var audio = new Audio("ressources/audio/TP_SaveMenu_Continue_No.wav");
     audio.play();
     setTimeout(function () {
         restore();
@@ -69,8 +69,7 @@ function quit() {
 }
 
 function restore() {
-    var body = document.querySelector('body');
-    body.innerHTML = first_body;
+    $("body").html(first_body);
 }
 
 function display_sheets_ocarina() {
